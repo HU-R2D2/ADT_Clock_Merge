5b49795ef6771f7ae0f6e6925375ce3bea64fcd9
Aydin Biber <biber.aydin@gmail.com>
2016-06-10 10:18:10 +0200

    Fagan inspection changes to clock
    Fixed the mistakes found during the fagan inspection for Clock.hpp and Clock.cpp

--- source/include/Clock.hpp
+++ source/include/Clock.hpp
@@ -7,7 +7,8 @@
 #include "timestamp.hpp"
 
 /// The main Clock object, which holds Clock::Duration and Clock::TimeStamp
-/// The Clock class is the main class which will be used to refer to Clock::Duration and Clock::TimeStamp.
+/// The Clock class is the main class which will be used
+/// to refer to Clock::Duration and Clock::TimeStamp.
 /// \author Thijs Hendrickx, Zehna van den Berg, Waila Woe, Aydin Biber
 /// \version 1.0
 /// \date 31-3-2016
